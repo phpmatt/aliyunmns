cd2b3edcaa94eb1a96bf4beb2671be0adddab6fa
liuguoqiang <308662921@qq.com>
2017-08-03 11:11:05 +0800

    add aliyunmns

--- AliMns.php
+++ AliMns.php
@@ -2,12 +2,30 @@
 namespace phpmatt\aliyunmns;
 require_once(__DIR__.'/src/mns-autoloader.php');
 
-use yii\base\Widget;
+use yii\base\Controller;
+use AliyunMNS\Client;
+use AliyunMNS\Topic;
+use AliyunMNS\Model\MailAttributes;
+use AliyunMNS\Model\SmsAttributes;
+use AliyunMNS\Model\BatchSmsAttributes;
+use AliyunMNS\Model\MessageAttributes;
+use AliyunMNS\Exception\MnsException;
+use AliyunMNS\Requests\PublishMessageRequest;
 
-class AliMns extends Widget
+class AliMns extends Controller
 {
-    public function run()
-    {
-        return "Hello!";
-    }
+  public $endPoint = '';
+  public $accessId = '';
+  public $accessKey = '';
+  public static $client;
+
+  public function __construct($endPoint, $accessId, $accessKey)
+  {
+    $this->client = new Client($endPoint, $accessId, $accessKey);
+  }
+
+  public static function Send()
+  {
+    return self::$client;
+  }
 }
